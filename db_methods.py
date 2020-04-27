f26b39410f0abd4f1f26d07e759f3928fa7a9264
kosted <konstantinus@gmail.com>
2020-04-27 15:23:03 +0300

    finally complete dinamic select and insert db methods

--- db_methods.py
+++ db_methods.py
@@ -12,6 +12,13 @@ global connection
 global cursor
 
 
+def ident_or_literal(s):
+    if type(s) == str:
+        return sql.Identifier(s)
+    else:
+        return sql.Literal(s)
+
+
 def open_connection():
     global connection
     global cursor
@@ -57,20 +64,22 @@ def init_db():
             return False
 
 
-def select_request(columns='*', tables=('guild',), limit=(), where=()):
+def select_request(columns='*', tables=('guild',), limit=(), where={}):
     if columns == "*":
         sql_command = "SELECT * FROM {tables}"
     else:
         sql_command = "SELECT {columns} FROM {tables}"
     if where:
-        sql_command += " WHERE {where}"
+        if where['sign'] in ["=", '>', '<', 'LIKE']:
+            sql_command += " WHERE {where_field} " + where["sign"] + " {where_value}"
     if limit:
         sql_command += " LIMIT {limit}"
     sql_command += ';'
-    sql_command = sql.SQL(sql_command).format(columns=sql.SQL(", ".join([sql.Identifier(column) for column in columns])),
-                                              tables=sql.SQL(", ".join([sql.Identifier(table) for table in tables])),
-                                              where=sql.SQL(", ".join([sql.Identifier(column) for column in where])),
-                                              limit=sql.Literal(AsIs(limit).__str__()))
+    sql_command = sql.SQL(sql_command).format(columns=sql.SQL(", ").join(sql.Identifier(column) for column in columns),
+                                              tables=sql.SQL(", ").join([sql.Identifier(table) for table in tables]),
+                                              where_field=sql.Identifier(where["where_field"]),
+                                              where_value=sql.Literal(where["where_value"]),
+                                              limit=sql.Literal(limit))
     cursor.execute(sql_command)
     #    {'columns': AsIs(columns),
     # 'tables': AsIs(tables),
@@ -84,33 +93,43 @@ def select_request(columns='*', tables=('guild',), limit=(), where=()):
 def insert_request(values, columns=(), table='guild'):
     # cursor.execute("INSERT INTO %(table)s VALUES( %(values1)s,%(values2)s, %(values3)s);", AsIs(tables))
 
+    # c обьявлением колонок
     if columns is not None:
-        sql_command = "INSERT INTO {table} ({columns}) VALUES (%(values)s);"
+        sql_command = "INSERT INTO {table} ({columns}) VALUES {values};"
 
+        # если вставляется не одно значение в таблицу
         if type(values[0]) in (list, tuple):
-
+            pre_value = sql.SQL("")
             for value in values:
-                cursor.execute(sql.SQL(sql_command).format(table=sql.Identifier(table),
-                                                           columns=sql.Identifier(", ".join(columns))),
-                               AsIs(value).__str__())
+                if pre_value != sql.SQL(""):
+                    pre_value += sql.SQL(", ")
+                pre_value += sql.SQL("(") + sql.SQL(", ").join(sql.Literal(value_part) for value_part in value) + sql.SQL(")")
+            print(pre_value.as_string(connection))
+            cursor.execute(sql.SQL(sql_command).format(table=sql.Identifier(table),
+                                                       columns=sql.SQL(", ").join(
+                                                           [sql.Identifier(column) for column in columns]),
+                                                       values=pre_value))
+        # вставка одного значения
         else:
             cursor.execute(sql.SQL(sql_command).format(table=sql.Identifier(table),
-                                                       columns=sql.Identifier(", ".join(columns)),
-                                                       values=", ".join(values)))
-    else:
-        sql_command = "INSERT INTO %(table)s VALUES ( %(values)s );"
+                                                       columns=sql.SQL(", ").join(
+                                                           [sql.Identifier(column) for column in columns]),
+                                                       values=sql.SQL("(") + sql.SQL(", ").join(
+                                                           sql.Literal(value) for value in values) + sql.SQL(")")))
+            # без обьявления колонок
+        # else:
+        #     sql_command = "INSERT INTO %(table)s VALUES ( %(values)s );"
+        #
+        #     values = (", ".join(value) for value in values)
+        #     cursor.execute(sql_command, {'values': AsIs(values),
+        #                                  "table": AsIs(table)})
+        #     # else:
+        #     #     insert_method(sql_command, {'values': AsIs(", ".join(values)),
+        #     #                                 "table": AsIs(table)})
+        connection.commit()
 
-        values = (", ".join(value) for value in values)
-        cursor.execute(sql_command, {'values': AsIs(values),
-                                     "table": AsIs(table)})
-    # else:
-    #     insert_method(sql_command, {'values': AsIs(", ".join(values)),
-    #                                 "table": AsIs(table)})
-
-
-    connection.commit()
 
 if __name__ == "__main__":
-    open_connection()
-    init_db()
-    close_connection()
+            open_connection()
+            init_db()
+            close_connection()
